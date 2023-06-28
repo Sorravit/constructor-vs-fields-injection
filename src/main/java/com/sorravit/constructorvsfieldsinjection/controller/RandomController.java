d953a4347fd1f8b8f1c53d58b046cc544a2b2dca
Big <sorravit.bunjongpean@aia.com>
2023-06-28 14:59:58 +0700

    Add validation to address Model * Need to add         <dependency>             <groupId>org.springframework.boot</groupId>             <artifactId>spring-boot-starter-validation</artifactId>         </dependency> to make it work

--- src/main/java/com/sorravit/constructorvsfieldsinjection/controller/RandomController.java
+++ src/main/java/com/sorravit/constructorvsfieldsinjection/controller/RandomController.java
@@ -2,9 +2,14 @@ package com.sorravit.constructorvsfieldsinjection.controller;
 
 import com.sorravit.constructorvsfieldsinjection.model.Address;
 import com.sorravit.constructorvsfieldsinjection.model.Book;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class RandomController {
     @GetMapping("address")
@@ -15,12 +20,23 @@ public class RandomController {
         return address;
     }
 
+    @PostMapping("address")
+    Address postAddress(@Valid @RequestBody Address address) {
+        log.warn(String.valueOf(address));
+        return address;
+    }
+
     @GetMapping("book")
-    Book getBook(){
+    Book getBook() {
         Book book = new Book();
         book.setTitle("this is the book");
         book.setPrice(69.112);
         return book;
+    }
 
+    @PostMapping("book")
+    Book postBook(@RequestBody Book book) {
+        log.warn(String.valueOf(book));
+        return book;
     }
 }
--- src/main/java/com/sorravit/constructorvsfieldsinjection/model/Address.java
+++ src/main/java/com/sorravit/constructorvsfieldsinjection/model/Address.java
@@ -1,16 +1,19 @@
 package com.sorravit.constructorvsfieldsinjection.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 @Schema(description = "Address details")
 public class Address {
-
     @Schema(description = "The address", example = "11 Street")
     private String address;
 
-    @Schema(description = "The postal code", example = "1150")
+    @NotEmpty
+    @Size(min = 5, max = 5)
+    @Schema(description = "Five digit postal code", example = "11500")
     private String postCode;
 
     // Getters and setters
